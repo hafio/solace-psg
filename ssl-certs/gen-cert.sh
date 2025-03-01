f7d40ed23809e1ffc770bc3dd15b76828d80431d
Hamlyn <github@hafio.tv>
2025-03-01 10:01:37 +0800

    fixed script to include SAN in server certificate

--- ssl-certs/gen-cert.sh
+++ ssl-certs/gen-cert.sh
@@ -2,12 +2,12 @@
 
 ## Generates a new key and certificate for root CA, then generates a new key and certificate for server
 
-export ROOT_PASS="5e_E(Zf9l2f!v4pRx2V%" # export required as openssl command is crafted to use environment variables
+export ROOT_PASS= # export required as openssl command is crafted to use environment variables
 ROOT_CN=authority.handy
 
 export PASS= # export required as openssl command is crafted to use environment variables
 CN=solace.handy
-SAN="DNS:*.handy,DNS:*.local,IP:10.10.10.188, IP:10.10.10.189"
+export SAN="DNS:*.handy,DNS:*.local,IP:10.10.10.188,IP:10.10.10.189" # export required as openssl command is crafted to use environment variables
 CTRY=SG
 ST=
 LOC="Handy Road"
@@ -22,6 +22,23 @@ concatIfNotBlank() {
 		echo "/$1=$2"
 	fi
 }
+passwordOrBlank() {
+	if [[ -n "${!1}" ]]; then
+		echo "-passout env:$1"
+	else
+		echo "-nodes"
+	fi
+}
+checkExit() {
+	if [[ $1 -ne 0 ]]; then
+		echo "Failed"
+		exit
+	else
+		echo "Ok"
+	fi
+}
+PARM_ROOT_PASS=`passwordOrBlank ROOT_PASS`
+PARM_PASS=`passwordOrBlank PASS`
 
 ROOT_CN=`concatIfNotBlank CN "${ROOT_CN}"`
 CN=`concatIfNotBlank CN "${CN}"`
@@ -34,26 +51,39 @@ EMAIL=`concatIfNotBlank emailAddress "${EMAIL}"`
 SUBJ="${CN}${CTRY}${ST}${LOC}${ORG}${ORG_UNIT}${EMAIL}"
 RSUBJ="${ROOT_CN}${CTRY}${ST}${LOC}${ORG}${ORG_UNIT}${EMAIL}"
 
+echo '[v3_req]
+keyUsage = keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = $ENV::SAN' > server.conf
+
 if [[ "$1" == "ec*" ]]; then
-	echo "Generating Root Certificate and Key..."
-	openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -days 3650 -keyout CA.key -out CA.crt -passout env:ROOT_PASS -subj "${RSUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
+	echo -n "Generating Root Certificate and Key..."
+	openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -days 3650 -keyout CA.key -out CA.crt ${PARM_ROOT_PASS} -subj "${RSUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
+	checkExit $?
 	echo "${ROOT_PASS}" > CA.pass
 	
-	echo "Generating Server CSR and Key..."
-	openssl ecparam -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -passout env:PASS -keyout server.key -out server.csr -subj "${SUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
+	echo -n "Generating Server CSR and Key..."
+	openssl ecparam -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 ${PARM_PASS} -keyout server.key -out server.csr -subj "${SUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
+	checkExit $?
 	echo "${PASS}" > server.pass
 	
-	echo "Generating Server Certificate and Key..."
-	openssl x509 -req -in server.csr -CA CA.crt -CAkey CA.key -CAcreateserial -passin env:ROOT_PASS -sha256 -days 365 -out server.crt
+	echo -n "Generating Server Certificate and Key..."
+	openssl x509 -req -in server.csr -CA CA.crt -CAkey CA.key -CAcreateserial -passin env:ROOT_PASS -sha256 -days 365 -out server.crt -extfile server.conf -extensions v3_req
+	checkExit $?
+
 else
-	echo "Generating Root Certificate and Key..."
-	openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -keyout CA.key -out CA.crt -passout env:ROOT_PASS -subj "${RSUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
+	echo -n "Generating Root Certificate and Key..."
+	openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -keyout CA.key -out CA.crt ${PARM_ROOT_PASS} -subj "${RSUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
+	checkExit $?
 	echo "${ROOT_PASS}" > CA.pass
 	
-	echo "Generating Server CSR and Key..."
-	openssl req -newkey rsa:4096 -passout env:PASS -keyout server.key -out server.csr -subj "${SUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
+	echo -n "Generating Server CSR and Key..."
+	openssl req -newkey rsa:4096 ${PARM_PASS} -keyout server.key -out server.csr -subj "${SUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
+	checkExit $?
 	echo "${PASS}" > server.pass
 	
-	echo "Generating Server Certificate and Key..."
-	openssl x509 -req -in server.csr -CA CA.crt -CAkey CA.key -CAcreateserial -passin env:ROOT_PASS -sha256 -days 365 -out server.crt
+	echo -n "Generating Server Certificate..."
+	openssl x509 -req -in server.csr -CA CA.crt -CAkey CA.key -CAcreateserial -passin env:ROOT_PASS -sha256 -days 365 -out server.crt -extfile server.conf -extensions v3_req
+	checkExit $?
+	
 fi
