7c226c35a791e3255ccaf1de668426784b0d8874
Hamlyn <github@hafio.tv>
2025-03-12 13:51:43 +0800

    updated gen-cert.sh to include ssl extensions for certificate. this is required for more modern browsers.

--- ssl-certs/README.md
+++ ssl-certs/README.md
@@ -1,21 +1,50 @@
 # Generate Root Certificate + Server Certificate
 
-This project generates a root certificate (for the Certificate Authority) and server certificate signed by the generate CA cert.
+This project generates a root certificate (for the Certificate Authority) and server certificate signed by the generated CA cert.
+
+`gen-cert.sh` generates the certificates without additional/temporary configuration files. However, if the generated certificate does not produce expected results, please check/compare openssl configuration file `/etc/ssl/openssl.cnf` in case your system has different default values.
 
 Read https://deliciousbrains.com/ssl-certificate-authority-for-local-https-development/ for more information or google/chatbot for more information. Information about openssl and its documentation is surprisingly obstrufcated - I recommend taking an online course (e.g. Udemy etc) to understand in more details.
 
-Please note that wildcard certificates only apply for third-level domains and onwards.
+# Key OpenSSL information to take note
+
+1. Wildcard certificates only apply for third-level domains and onwards.
+2. To carry over x509 extensions (e.g. SAN, keyUsage, etc.) in CSR for CA-signed certificates, use `-copy_extensions copy` when generating the certificate [OpenSSL x509 Doc](https://docs.openssl.org/3.0/man1/openssl-x509/).
+   - Take note that usual Certificate Authority do not use `-copy_extensions` as the CSR can contain insecured parameters (e.g. CA:TRUE).
+   - using `-ext` in combination with `-copy_extensions` can specify the list of extensions to copy over. Refer to [x509_config](https://docs.openssl.org/3.2/man5/x509v3_config/) for list of values.
+3. Modern browsers require extensions `keyUsage` & `extendedKeyUsage` to be specified correctly.
+4. `openssl` configuration file `/etc/ssl/openssl.cnf` might have conflicting / overriding configuration specified. Please check the file if generated certificate does not align with specified parameters.
+5. OpenSSL trust settings are only used for root certificates. Future versions of OpenSSL will recognize this in all certificates.
+
 
 # `gen-cert.sh`
 
 ```bash
-Usage: gen-cert.sh [ecc] [OPTIONS]
-	[ecc] : if this is specified, certificated will be generated with Elliptical Curve Cryptography (ECC) instead of RSA.
+Usage: gen-cert.sh [OPTIONS]
+
+    This script will generate certificate (.crt), key (.crt), and password (.pass) for both CA and server, and certificate signing request (.csr) for server.
+
+        Password file will not be generated if password is empty. By default, scripts does not use any password for certificate generation/signing.
+
+        Script utillizes only RSA algorithm.
+
+        Documentation: https://github.com/hafio/solace-psg/tree/main/ssl-certs
+
   Options:
-    --root-cert-prefix [prefix] : Script will append prefix with '.crt' and '.key' for '-CA' and '-CAkey' used to sign server certificate. Value cannot be 'server'.
-    --root-pass [pass] : Password to be used for root certificate key. Leave out parameter to not use any password.
-    --server-pass [pass] : Password to be used for server certificate key. Leave out parameter to not use any password."
+    --root-cert-name <name> : Root certificate output filename prefix.
+    --root-cert-prefix <prefix> : Root certificate input filename prefix. This option will skip root certificate creation.
+    --root-pass <pass> : Password to be used for root certificate key.
+    --server-cert-name <prefix> : Server certificate output filename prefix.
+    --server-pass <pass> : Password to be used for server certificate key.
 ```
+
+| Script parameter     | Description                                                                                                                                                                                       |
+|----------------------|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| `--root-cert-name`   | Used to specify the root certificate output filename prefix for .crt, .key, .pass files. e.g. `--root-cert-name CA-cert` will produce `CA-cert.crt` `CA-cert.key` `CA-cert.pass`.                 |
+| `--root-cert-prefix` | Used to specify the root certificate filename prefix for a root certificate that was previously generated. This option, when specified, will skip root certificate generation.                    |
+| `--root-pass`        | Used to specify root certificate password. This is used to specify root password to be used for root certificate generation and also the root input password used to sign the server certificate. |
+| `--server-cert-name` | Used to specify the server certificate output filename prefix for .crt, .key, .pass files. e.g. `--server-cert-name svr-cert` will produce `svr-cert.crt` `svr-cert.key` `svr-cert.pass`.         |
+| `--server-pass`      | Used to specify the server certificate password.                                                                                                                                                  |
 
 All certificate details (CN, Org, OU, etc) including password are specified as configuration within the bash script. Please update them accordingly
 
--- ssl-certs/gen-cert.sh
+++ ssl-certs/gen-cert.sh
@@ -2,60 +2,128 @@
 
 ## Generates a new key and certificate for root CA, then generates a new key and certificate for server
 
-ROOT_CN=authority.handy
-CN=hamaster.handy
-export SAN="DNS:*.hamaster.handy,DNS:*.hasol.handy,IP:10.10.10.188,IP:10.10.10.189" # export required as openssl command is crafted to use environment variables
-CTRY=SG
-ST=
-LOC="Handy Road"
-ORG=Solace
-ORG_UNIT="Professional Services"
-EMAIL="hamlyn.yew@solace.com"
+ROOT_CN="CA Authority"
+ROOT_CTRY=SG
+ROOT_ST=
+ROOT_LOC="Singapore"
+ROOT_ORG="Solace"
+ROOT_ORG_UNIT="IT"
+ROOT_EMAIL=
+ROOT_SAN="DNS:authority.handy,DNS:*.hamaster.handy,DNS:*.hasol.handy"
 
-############# CHANGE VALUES ABOVE ##################
+SVR_CN="Hamaster"
+SVR_CTRY=SG
+SVR_ST=
+SVR_LOC="Handy Road"
+SVR_ORG="HAFIO"
+SVR_ORG_UNIT="Home"
+SVR_EMAIL="home@hafio.tv"
+SVR_SAN="DNS:hamaster.handy,DNS:*.hamaster.handy,DNS:*.hasol.handy"
+
+########################################################################################
+############################## CHANGE CONFIG VALUES ABOVE ##############################
+########################################################################################
 
 echoUsage() {
-	echo "Usage: gen-cert.sh [ecc] [OPTIONS]
-	[ecc] : if this is specified, certificated will be generated with Elliptical Curve Cryptography (ECC) instead of RSA.
+	echo "Usage: gen-cert.sh [OPTIONS]
+	
+    This script will generate certificate (.crt), key (.crt), and password (.pass) for both CA and server, and certificate signing request (.csr) for server.
+	
+	Password file will not be generated if password is empty.
+	
+	Script utillizes only RSA algorithm.
+	
+	Documentation: https://github.com/hafio/solace-psg/tree/main/ssl-certs
+	
   Options:
-    --root-cert-prefix [prefix] : Script will append prefix with '.crt' and '.key' for '-CA' and '-CAkey' used to sign server certificate. Value cannot be 'server'.
-    --root-pass [pass] : Password to be used for root certificate key. Leave out parameter to not use any password.
-    --server-pass [pass] : Password to be used for server certificate key. Leave out parameter to not use any password."
-	exit
+    --root-cert-name <name> : Root certificate output filename prefix.
+    --root-cert-prefix <prefix> : Root certificate input filename prefix. This option will skip root certificate creation.
+    --root-pass <pass> : Password to be used for root certificate key.
+    --server-cert-name <prefix> : Server certificate output filename prefix.
+    --server-pass <pass> : Password to be used for server certificate key."
+	exit 1
 }
-concatIfNotBlank() {
+concatCnIfNotBlank() {
 	[[ -n "$2" ]] && echo "/$1=$2"
 }
 passwordOrBlank() {
 	[[ -n "${!1}" ]] && echo "-passout env:$1" || echo "-nodes"
 }
 checkExit() {
-	[[ $1 -ne 0 ]] && (
+	if [[ $1 -ne 0 ]] || [[ ! -f "$2" ]]; then
 		echo "Failed"
-		exit
-	) || echo "Ok"
+		exit 1
+	else
+		echo "Ok : $2"
+	fi
 }
+
+ALGO="rsa:4096" # use 4096 and above as 2048 will not be acceptable beyond 2030
+ROOT_DAYS=3650
+SVR_DAYS=365
 USE_EC=false
-ROOT_CERT_PREFIX=""
+GEN_ROOT=1
 export ROOT_PASS=""
-export SERVER_PASS=""
+export SVR_PASS=""
+
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    ec*)
-      USE_EC=true
-      shift
-      ;;
+	--root-cert-name)
+	  if [[ -z "$2" ]]; then
+		echo "$1: Missing filename prefix"
+		exit 1
+	  fi
+	  if [[ ${GEN_ROOT} -eq 0 ]]; then
+		echo "--root-cert-name cannot be specified together with --root-cert-prefix"
+		exit 1
+	  fi
+	  ROOT_FILE_PREFIX="$2"
+	  
+	  shift 2
+	  ;;
     --root-cert-prefix)
-      ROOT_CERT_PREFIX="$2"
-	  [[ "${ROOT_CERT_PREFIX}" == "server" ]] && (echo "Root Certificate Prefix cannot be server"; exit)
+	  if [[ -z "$2" ]]; then
+		echo "$1: Missing filename prefix"
+		exit 1
+	  fi
+	  if [[ -n "${ROOT_FILE_PREFIX}" ]]; then
+		echo "--root-cert-prefix cannot be specified together with --root-cert-name"
+		exit 1
+	  fi
+	  if [[ "$2" == "server" ]] || [[ "${SVR_FILE_PREFIX}" == "$2" ]]; then
+		echo "--root-cert-prefix cannot be 'server' or the same as --server-cert-name"
+		exit 1
+	  fi
+	  GEN_ROOT=0
+      ROOT_FILE_PREFIX="$2"
       shift 2
       ;;
+	--server-cert-name)
+	  if [[ -z "$2" ]]; then
+		echo "$1: Missing filename prefix"
+		exit 1
+	  fi
+	  if [[ "$2" == "CA" ]] || [[ "$2" == "${ROOT_FILE_PREFIX}" ]]; then
+		echo "--server-cert-name cannot be 'CA' or the same as --root-cert-name"
+		exit 1
+	  fi
+	  SVR_FILE_PREFIX="$2"
+	  shift 2
+	  ;;
     --root-pass)
-      export ROOT_PASS="$2"
+	  if [[ -z "$2" ]]; then
+		echo "$1: Missing password"
+		exit 1
+	  fi
+      ROOT_PASS="$2"
       shift 2
       ;;
     --server-pass)
-      export SERVER_PASS="$2"
+	  if [[ -z "$2" ]]; then
+		echo "$1: Missing password"
+		exit 1
+	  fi
+      SVR_PASS="$2"
       shift 2
       ;;
     *)
@@ -64,47 +132,53 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-ROOT_CERT=${ROOT_CERT_PREFIX:-CA}.crt
-ROOT_KEY=${ROOT_CERT_PREFIX:-CA}.key
+ROOT_FILE_PREFIX=${ROOT_FILE_PREFIX:-CA}
+SVR_FILE_PREFIX=${SVR_FILE_PREFIX:-server}
 PARM_ROOT_PASS=`passwordOrBlank ROOT_PASS`
-PARM_PASS=`passwordOrBlank PASS`
+PARM_SVR_PASS=`passwordOrBlank SVR_PASS`
 
-ROOT_CN=`concatIfNotBlank CN "${ROOT_CN}"`
-CN=`concatIfNotBlank CN "${CN}"`
-CTRY=`concatIfNotBlank C "${CTRY}"`
-ST=`concatIfNotBlank ST "${ST}"`
-LOC=`concatIfNotBlank L "${LOC}"`
-ORG=`concatIfNotBlank O "${ORG}"`
-ORG_UNIT=`concatIfNotBlank OU "${ORG_UNIT}"`
-EMAIL=`concatIfNotBlank emailAddress "${EMAIL}"`
-SUBJ="${CN}${CTRY}${ST}${LOC}${ORG}${ORG_UNIT}${EMAIL}"
-RSUBJ="${ROOT_CN}${CTRY}${ST}${LOC}${ORG}${ORG_UNIT}${EMAIL}"
+RCN=`concatCnIfNotBlank CN "${ROOT_CN}"`
+RCTRY=`concatCnIfNotBlank C "${ROOT_CTRY}"`
+RST=`concatCnIfNotBlank ST "${ROOT_ST}"`
+RLOC=`concatCnIfNotBlank L "${ROOT_LOC}"`
+RORG=`concatCnIfNotBlank O "${ROOT_ORG}"`
+RORG_UNIT=`concatCnIfNotBlank OU "${ROOT_ORG_UNIT}"`
+REMAIL=`concatCnIfNotBlank emailAddress "${ROOT_EMAIL}"`
+RSUBJ="${RCN}${RCTRY}${RST}${RLOC}${RORG}${RORG_UNIT}${REMAIL}"
 
-echo 'keyUsage = keyEncipherment, dataEncipherment
-extendedKeyUsage = serverAuth
-subjectAltName = $ENV::SAN' > .server.conf
+SCN=`concatCnIfNotBlank CN "${SVR_CN}"`
+SCTRY=`concatCnIfNotBlank C "${SVR_CTRY}"`
+SST=`concatCnIfNotBlank ST "${SVR_ST}"`
+SLOC=`concatCnIfNotBlank L "${SVR_LOC}"`
+SORG=`concatCnIfNotBlank O "${SVR_ORG}"`
+SORG_UNIT=`concatCnIfNotBlank OU "${SVR_ORG_UNIT}"`
+SEMAIL=`concatCnIfNotBlank emailAddress "${SVR_EMAIL}"`
+SSUBJ="${SCN}${SCTRY}${SST}${SLOC}${SORG}${SORG_UNIT}${SEMAIL}"
 
 if ${USE_EC}; then
 	echo "Not implemented"
 	exit 1
 
 else
-	if [[ -z ${ROOT_CERT_PREFIX} ]]; then
+	if [[ ${GEN_ROOT} -ne 0 ]]; then
 		echo -n "Generating Root Certificate and Key..."
-		openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -keyout ${ROOT_KEY} -out ${ROOT_CERT} ${PARM_ROOT_PASS} -subj "${RSUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
-		checkExit $?
-		echo "${ROOT_PASS}" > ${ROOT_CERT_PREFIX:-CA}.pass
+		openssl req -x509 -newkey ${ALGO} -sha256 -days ${ROOT_DAYS} -keyout ${ROOT_FILE_PREFIX}.key -out ${ROOT_FILE_PREFIX}.crt ${PARM_ROOT_PASS} -subj "${RSUBJ}" -addext "subjectAltName=${ROOT_SAN}" -addext "basicConstraints=critical,CA:TRUE,pathlen:1" -addext "keyUsage=critical,keyCertSign,cRLSign,digitalSignature" 2> /dev/null
+		checkExit $? ${ROOT_FILE_PREFIX}.crt
+		[[ -n ${ROOT_PASS} ]] && echo "${ROOT_PASS}" > ${ROOT_FILE_PREFIX}.pass
 	fi
 	
-	echo -n "Generating Server CSR and Key..."
-	openssl req -newkey rsa:4096 ${PARM_PASS} -keyout server.key -out server.csr -subj "${SUBJ}" -addext "subjectAltName=${SAN}" 2> /dev/null
-	checkExit $?
-	echo "${PASS}" > server.pass
-	
-	echo -n "Generating Server Certificate..."
-	openssl x509 -req -in server.csr -CA ${ROOT_CERT} -CAkey ${ROOT_KEY} -CAcreateserial -passin env:ROOT_PASS -sha256 -days 365 -out server.crt -extfile .server.conf
-	checkExit $?
-	
-	rm .server.conf
-	
+	if [[ -f "${ROOT_FILE_PREFIX}".crt ]] && [[ -f "${ROOT_FILE_PREFIX}.key" ]]; then
+		echo -n "Generating Server CSR and Key..."
+		openssl req -newkey ${ALGO} ${PARM_SVR_PASS} -keyout ${SVR_FILE_PREFIX}.key -out ${SVR_FILE_PREFIX}.csr -subj "${SSUBJ}" -addext "subjectAltName=${SVR_SAN}" -addext "basicConstraints=critical,CA:FALSE" -addext "keyUsage=critical,digitalSignature,keyEncipherment,dataEncipherment" -addext "extendedKeyUsage=critical,serverAuth" 2> /dev/null
+		checkExit $? ${SVR_FILE_PREFIX}.csr
+		[[ -n ${SVR_PASS} ]] && echo "${SVR_PASS}" > ${SVR_FILE_PREFIX}.pass
+		
+		echo -n "Generating Server Certificate..."
+		openssl x509 -req -in ${SVR_FILE_PREFIX}.csr -CA ${ROOT_FILE_PREFIX}.crt -CAkey ${ROOT_FILE_PREFIX}.key -CAcreateserial -passin env:ROOT_PASS -sha256 -days ${SVR_DAYS} -copy_extensions copy -out ${SVR_FILE_PREFIX}.crt 2> /dev/null
+		checkExit $? ${SVR_FILE_PREFIX}.crt
+		openssl x509 -noout -in ${SVR_FILE_PREFIX}.crt -issuer -subject -ext subjectAltName
+	else
+		echo "Root file(s) (${ROOT_FILE_PREFIX}.crt and/or ${ROOT_FILE_PREFIX}.key) is/are missing"
+		exit 1
+	fi
 fi
