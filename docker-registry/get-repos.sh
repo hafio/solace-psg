937da25411fe23472404ff4ead86c02bc60ff338
Hamlyn <github@hafio.tv>
2025-04-27 20:32:41 +0800

    correct bash function position

--- docker-registry/get-repos.sh
+++ docker-registry/get-repos.sh
@@ -2,6 +2,15 @@
 
 REG_URL=https://registry.hamaster.handy:45443
 
+echoUsage() {
+	echo "Usage: $0 [OPTIONS]"
+	echo 'OPTIONS:
+	--tags <repo> : list available tags of Repository
+	--CA-cert <file> : specify CA certificate file to use for registries using non-public CAs
+	--url <url> : specify registry url to connect to instead of using configured url'
+	exit
+}
+
 while [[ $# -gt 0 ]]; do
   case "$1" in
     --tags)
@@ -22,16 +31,5 @@ while [[ $# -gt 0 ]]; do
       ;;
   esac
 done
-
-echoUsage() {
-	echo "Usage: $0 [OPTIONS]"
-	echo 'OPTIONS:
-	--tags <repo> : list available tags of Repository
-	--CA-cert <file> : specify CA certificate file to use for registries using non-public CAs
-	--url <url> : specify registry url to connect to instead of using configured url'
-	exit
-}
-
-
 
 [[ "${SHOW_TAGS}" == "true" ]] && (curl ${PARM_CA} -s "${REG_URL}/v2/${REPO}/tags/list" | jq .tags) || (curl ${PARM_CA} -s "${REG_URL}/v2/_catalog"	| jq .repositories)
